180ab70fd0523832ad60067dcdeb9ae224fece11
Suraj Das <44750499+thesurajdas@users.noreply.github.com>
2022-06-12 11:22:23 +0530

    Update index.php
    add slider in index page

--- index.php
+++ index.php
@@ -23,17 +23,17 @@
   </head>
   <body>
     <main>
-      <!-- <img src="img/ab_banner.jpg" width="99.7%" > -->
+      <!-- <img src="img/ab_banner.jpg" width="100%" > -->
       <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="img/ab_banner.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="img/ab_banner.jpg" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="img/ab_banner.jpg" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
