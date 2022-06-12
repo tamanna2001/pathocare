067601094ba7bb136fba00782694a938288e274f
tamanna2001 <islamtamanna445@gmail.com>
2022-06-12 12:45:25 +0530

    done

--- pages/services.php
+++ pages/services.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Doctors</title>
+    <title>Services</title>
     <?php require_once('../header.php'); ?>
 </head>
 <body>
